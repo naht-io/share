c028640926bd8f809db98eee5547e598dcbfe860
mara.webp <mara@froehlich.network>
2026-07-04 21:09:18 +0200

    add some vertical padding on desktop for big text

--- app/routes/layout.tsx
+++ app/routes/layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router";
 export default function Layout() {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-auto flex items-center justify-center p-4">
+      <div className="flex-auto flex items-center justify-center p-4 md:py-24">
         <Outlet />
       </div>
     </div>
